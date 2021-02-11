1d65794433235fc8671d7c7cd7f6687254fc5599
Kwuffin <bestboykevin.bbg@gmail.com>
2021-02-11 15:28:52 +0100

    Added more documentation and docstrings

--- simple_alg.py
+++ simple_alg.py
@@ -3,14 +3,19 @@ from random import choice
 
 
 def simple_algorithm(answer):
+    """
+    Simple algorithm uit de 'YET ANOTHER MASTERMIND STRATEGY' van de Universiteit Groningen.
+
+    :param answer: Secret code
+    """
     possibilities = []
 
     # Creates all possible answers in order [1, 1, 1, 1] ... [6, 6, 6, 6]
     for i in range(1, 7):
         for j in range(1, 7):
             for k in range(1, 7):
-                for l in range(1, 7):
-                    possibilities.append([i, j, k, l])
+                for m in range(1, 7):
+                    possibilities.append([i, j, k, m])
 
     correct = False
 
@@ -18,22 +23,23 @@ def simple_algorithm(answer):
     while not correct:
         # Guesses the first possible guess in the sorted list of possibilities
         guess = possibilities[0]
-
-        count += 1
+        count += 1  # Counts amount of guesses
 
         print(f"Compuer gokt: {guess}")
 
+        # Feedback
         black, white = evaluate(answer, guess)
 
         # If all pins are in the correct place:
         if black == 4:
             correct = True
 
-        result = []
+        # Eliminates possibilities
+        new_pos = []
         for possibility in possibilities:
             if evaluate(possibility, guess) == (black, white):
-                result.append(possibility)
+                new_pos.append(possibility)
 
-        possibilities = result
+        possibilities = new_pos
 
     print(f"De computer heeft de secret gegokt in {count} gokken!")
