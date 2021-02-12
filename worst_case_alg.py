82a21f1a6641d0a05dc8eec4d755c683eb177033
Kwuffin <bestboykevin.bbg@gmail.com>
2021-02-12 20:56:23 +0100

    Updated documentation

--- worst_case_alg.py
+++ worst_case_alg.py
@@ -8,7 +8,6 @@ def worst_case_algorithm(answer):
     (2020, June 17). Stack Overflow.
     https://stackoverflow.com/questions/62430071/donald-knuth-algorithm-mastermind#:%7E:text=Mastermind%2DFive%2DGuess%2DAlgorithm,the%20number%20of%20possible%20patterns.
 
-
     :param answer: Secret code
     """
     possibilities = []
@@ -28,6 +27,7 @@ def worst_case_algorithm(answer):
         # Guesses the first possible guess in the sorted list of possibilities
         count += 1  # Counts amount of guesses
 
+        # If it's the first guess in the game.
         if count == 1:
             black, white = 0, 0
             guess = [1, 1, 2, 2]  # Initial guess
@@ -57,13 +57,30 @@ def worst_case_algorithm(answer):
 
 
 def minimax(possibilities, unused, old_black, old_white):
-    elim_dict = {}
+    """
+    Looks for the next guess that eliminates the most possibilities.
+    Does not work optimally as of now.
+
+    :param possibilities: List of possible codes
+    :param unused: List of all unused guesses
+    :param old_black: Amount of black pins from previous guess
+    :param old_white: Amount of white pins from previous guess
+
+    :return: Best next guess with the most eliminations
+    """
+    elim_dict = {}  # Dict that keeps track of which guess eliminates the most possibilities
+
+    # For each unused guess:
     for code in unused:
+
+        # Same elimination process as usual.
         new_posibilities = []
         for possibility in possibilities:
             if evaluate(possibility, code) == (old_black, old_white):
                 new_posibilities.append(possibility)
 
+        # Key: Guess converted to tuple because lists are not hashable, and thus not able to be dictionary keys.
+        # Value: The amount of eliminations
         elim_dict[tuple(code)] = len(possibilities) - len(new_posibilities)
 
-    return list(max(elim_dict, key=elim_dict.get))
+    return list(min(elim_dict, key=elim_dict.get))
